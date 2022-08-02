6a5f518c38fc955f6376d602c361176d04ea8583
GeorgeShug <george.shug.sakihama@gmail.com>
2022-08-02 14:18:49 -0300

    telas/telas pronta

--- app/src/main/java/com/nome_do_cliente/nurzed/TelaLogin.java
+++ app/src/main/java/com/nome_do_cliente/nurzed/TelaLogin.java
@@ -2,7 +2,10 @@ package com.nome_do_cliente.nurzed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaLogin extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class TelaLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        Button btlogar = findViewById(R.id.button2);
+
+        btlogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it =  new Intent(TelaLogin.this, TelaHome.class);
+            }
+        });
     }
 }
