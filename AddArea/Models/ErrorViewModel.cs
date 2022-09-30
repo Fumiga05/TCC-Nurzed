2ddec31e29c6f9dcbf031c7a1d6c01c60e48becc
Fumiga05 <104382367+Fumiga05@users.noreply.github.com>
2022-09-30 16:53:25 -0300

    Delete AddArea directory

--- AddArea/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace AddArea.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
