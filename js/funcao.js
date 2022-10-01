a1ea0562c3a68716179fc0738a40d1cd108dc073
Giovanna Alves <giovanna.alves848@gmail.com>
2022-09-30 21:51:29 -0300

    tent 23

---
+++ js/funcao.js
@@ -0,0 +1,82 @@
+function avaCronograma() {
+
+    let section = document.getElementById('aux30');
+    section.style.display = 'block';
+
+    let btnava = document.getElementById('aux10');
+    btnava.style.display = 'none';
+
+    let btntroc = document.getElementById('aux31');
+    btntroc.style.display = 'block';
+
+    let select = document.getElementById('aux9');
+    select.style.display = 'none';
+}
+
+function f5() {
+    if (window.confirm("Trocar de horário não salva suas alterações, ainda assim deseja trocar de horário?")) {
+        location.reload(true);
+    }
+}
+
+function avaPeriodo() {
+    let btnava2 = document.getElementById('aux36');
+    btnava2.style.display = 'none';
+
+    let btnTroca = document.getElementById('aux37');
+    btnTroca.style.display = 'block';
+
+    let conteudoPeriodo = document.getElementById('aux38');
+    conteudoPeriodo.style.display = 'block';
+
+    let vazio2 = document.getElementById('aux39');
+    vazio2.style.display = 'none';
+
+}
+
+function avaPeriodoADM() {
+    let btnava2 = document.getElementById('aux36');
+    btnava2.style.display = 'none';
+
+    let btnTroca = document.getElementById('aux37');
+    btnTroca.style.display = 'block';
+
+    let conteudoPeriodo = document.getElementById('aux38');
+    conteudoPeriodo.style.display = 'block';
+
+    let vazio2 = document.getElementById('aux47');
+    vazio2.style.display = 'none';
+
+}
+
+
+function validacaoData() {
+
+    var data = new Date();
+    var dia = String(data.getDate()).padStart(2, '0');
+    var mes = String(data.getMonth() + 1).padStart(2, '0');
+    var ano = data.getFullYear();
+    dataAtual = dia  +"/"+ mes +"/" + ano;
+    console.log(dataAtual);
+
+    var data = document.querySelector("#data_nascimento").value;
+    console.log(data);
+
+    var stringExemplo = "dataAtual";
+    var resultado = stringExemplo.split("/");
+    console.log(resultado);
+
+   /* var stringExemplo1 = "data";
+    var resultado1 = stringExemplo.split("/");
+    console.log(resultado1);*/
+
+  // if(data >= dataAtual){
+  //  alert("Data Invalida");
+  // }else{
+   // console.log("Data Valida");
+  // }
+
+
+}
+
+
